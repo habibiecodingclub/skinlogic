d1d04630aaa9b1e520e581064bda7110e6c886b1
Jesica Manurung <jesicamanurung08@gmail.com>
2025-12-11 14:27:39 +0800

    tes

--- resources/views/landing/sections/produk.blade.php
+++ resources/views/landing/sections/produk.blade.php
@@ -48,7 +48,7 @@
             </a>
         </div>
 
-        {{-- Grid Produk --}}
+        {{-- Grid Produk  tessssss--}}
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             
             @foreach($products as $product)
