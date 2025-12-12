bc774a5a5bb738b6ff6d868110793521625cc8bb
ilupii17 <asus07346@outlook.com>
2025-12-13 02:02:23 +0800

    page perawatan nya dulu cape gw

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 // 1. Route Home (WAJIB ADA ->name('home'))
-Route::get('/', function () {
-    return view('landing.index'); // Pastikan ini sesuai nama file view utama Anda
-})->name('home');
+Route::get("/", function () {
+    return view("landing.index"); // Pastikan ini sesuai nama file view utama Anda
+})->name("home");
 
 // 2. Route Halaman Produk
-Route::get('/produk', function () {
-    return view('landing.pages.semua-produk'); 
-})->name('produk.index');
+Route::get("/produk", function () {
+    return view("landing.pages.semua-produk");
+})->name("produk.index");
 
 // 3. Route Halaman Artikel
-Route::get('/artikel', function () {
-    return view('landing.pages.semua-artikel');
-})->name('artikel.index');
+Route::get("/artikel", function () {
+    return view("landing.pages.semua-artikel");
+})->name("artikel.index");
+
+Route::get("/perawatan", function () {
+    return view("landing.pages.perawatan");
+})->name("perawatan");
